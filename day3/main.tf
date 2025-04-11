9f83973da7772e2385922d81ef6be11fcddece21
EC2 Default User <ec2-user@ip-172-31-9-190.ap-south-1.compute.internal>
2025-04-11 08:17:58 +0000

    Add day3 Terraform Docker task

---
+++ day3/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.23.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_terraform"
+  image = docker_image.nginx.name
+
+
+
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
